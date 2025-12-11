455d439df62ff92696c87cbe2aee28c957c647f2
RakaZuhdi <rakazhp@gmail.com>
2025-12-10 19:01:43 -0500

    Made changes for deployment

--- Final_Submission/4_Web_Application/footy-liveliness-web/src/services/api.js
+++ Final_Submission/4_Web_Application/footy-liveliness-web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const API_URL = process.env.REACT_APP_API_URL || 'https://footyliveliness.onrender.com';
 
 export const fetchUpcomingMatches = async () => {
   const response = await axios.get(`${API_URL}/api/upcoming`);
